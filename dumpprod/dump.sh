4b6179f368b6b989454d4ad78a00b515af5755f5
Jason Bronson <Jbronson@gxm.com>
2024-08-21 15:43:56 -0400

    adjustments

--- dumpprod/dump.sh
+++ dumpprod/dump.sh
@@ -4,22 +4,22 @@
 # Set the database host as DB_HOST
 
 # Run pg_dump for ProdPlatform with verbose output
-pg_dump --no-password -U postgres -h $DB_HOST -p 15023 -Fc --verbose ProdPlatform > /var/www/dump/pokergo.dump
+pg_dump --no-password -U postgres -h $DB_HOST -p 15023 -Fc --verbose "ProdPlatform" > /var/www/dump/ProdPlatform.dump
 # Compress the dump file with high compression
-tar -czf /var/www/dump/prod.dump.tar.gz /var/www/dump/pokergo.dump
+tar -czf /var/www/dump/ProdPlatform.dump.tar.gz /var/www/dump/ProdPlatform.dump
 
 # Run pg_dump for ProdChat with verbose output
-pg_dump --no-password -U postgres -h $DB_HOST -p 15023 -Fc --verbose ProdChat > /var/www/dump/pokergochat.dump
+pg_dump --no-password -U postgres -h $DB_HOST -p 15023 -Fc --verbose "ProdChat" > /var/www/dump/ProdChat.dump
 # Compress the dump file with high compression
-tar -czf /var/www/dump/prodchat.dump.tar.gz /var/www/dump/pokergochat.dump
+tar -czf /var/www/dump/ProdChat.dump.tar.gz /var/www/dump/ProdChat.dump
 
 # Run pg_dump for ProdPGT with verbose output
-pg_dump --no-password -U postgres -h $DB_HOST -p 15023 -Fc --verbose ProdPGT > /var/www/dump/pgt.dump
+pg_dump --no-password -U postgres -h $DB_HOST -p 15023 -Fc --verbose "ProdPGT" > /var/www/dump/ProdPGT.dump
 # Compress the dump file with high compression
-tar -czf /var/www/dump/prodpgt.dump.tar.gz /var/www/dump/pgt.dump
+tar -czf /var/www/dump/ProdPGT.dump.tar.gz /var/www/dump/ProdPGT.dump
 
 # Run pg_dump for ProdPromo with verbose output
-pg_dump --no-password -U postgres -h $DB_HOST -p 15023 -Fc --verbose ProdPromo > /var/www/dump/promo.dump
+pg_dump --no-password -U postgres -h $DB_HOST -p 15023 -Fc --verbose "ProdPromo" > /var/www/dump/ProdPromo.dump
 # Compress the dump file with high compression
-tar -czf /var/www/dump/prodpromo.dump.tar.gz /var/www/dump/promo.dump
+tar -czf /var/www/dump/ProdPromo.dump.tar.gz /var/www/dump/ProdPromo.dump
 
